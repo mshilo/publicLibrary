0f114a0c877f3c69d7b1420f1f6dcebff3eb2470
maxshilo <mshilo92@gmail.com>
2017-12-01 20:24:32 +0200

    5

--- publicLibrary/app data/DbMain.cs
+++ publicLibrary/app data/DbMain.cs
@@ -3,10 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.Windows.Forms;
 
 namespace publicLibrary
 {
     class DbMain
     {
+        private OleDbConnection cnn = new OleDbConnection();
+        private OleDbCommand cmd = new OleDbCommand();
+        private DataSet ds = new DataSet();
+
+        public DbMain()
+        {
+            cnn.ConnectionString = "Provider= Microsoft.ACE.OLEDB.12.0;Data Source=..\\access\\LibraryDatabase.accdb ;Persist Security Info=False";
+        }
+
+        public DataSet GetAllRecord(string tableName)
+        {
+            ds = new DataSet();
+            cmd = new OleDbCommand();
+
+            try
+            {
+                cmd.CommandText = "SELECT * FROM " + tableName;
+                cmd.Connection = cnn;
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                da.Fill(ds);
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { cnn.Close(); }
+
+            return ds;
+        }
+
+        public DataSet GetQuery(string sql)
+        {
+            ds = new DataSet();
+            cmd = new OleDbCommand();
+
+            try
+            {
+                cmd.CommandText = sql;
+                cmd.Connection = cnn;
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                da.Fill(ds);
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { cnn.Close(); }
+
+            return ds;
+        }
+
+        public void Update(string sql)
+        {
+            /*          טענת כניסה: הפונקציה מקבלת מחרוזת פקודה
+                   טענת יציאה : הפונקציה מעדכנת את בסיס הנתונים       
+            */
+
+            cmd = new OleDbCommand();
+
+            try
+            {
+                cnn.Open();
+                cmd.Connection = cnn;
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { cnn.Close(); }
+        }
     }
 }
